8595185d3d167a858a969cee7fa62c6cb1b3f9ae
Beast <gambitbrian7m@gmail.com>
2023-07-22 18:55:12 +0300

    upload

---
+++ script.js
@@ -0,0 +1,128 @@
+const input = document.querySelector('.input');
+const submit = document.querySelector('.submit');
+
+const sectionTrue = document.querySelector('.app__main');
+const content = document.querySelectorAll('.content');
+
+const loc = document.querySelector('.location');
+const time = document.querySelector('.time');
+const image = document.querySelector('.image img');
+const temp = document.querySelector('.temp');
+const weather = document.querySelector('.weather');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+
+submit.onclick = () => {
+  search();
+}
+
+
+input.addEventListener('keypress', event => {
+  if (event.key.toLowerCase() === 'enter') {
+    search();
+  }
+})
+
+
+const search = function() {
+  let q = input.value;
+  
+  const apiKey = 'd81cb1596aa346d49c8213421232107';
+  const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${q}`
+  
+  fetch(query).then(response => response.json())
+    .then(data => {
+      console.log(data)
+      content[0].style.display = 'flex';
+      content[1].style.display = 'none';
+
+      sectionTrue.style.height = '580px'
+
+      content.forEach(block => {
+        block.style.opacity = '0';
+        block.style.scale = '60%';
+      });
+
+      loc.innerText = data.location.name;
+      time.innerText = data.location.localtime.split(' ')[1];
+      temp.innerText = `${data.current.temp_c}°C`;
+      weather.innerText = data.current.condition.text;
+      humidity.innerText = `Humidity: ${data.current.humidity}%`;
+      wind.innerText = `Wind: ${data.current.wind_kph}km/h`
+
+      if (data.current.is_day === 0) {
+        switch (data.current.condition.text) {
+          case 'Partly cloudy':
+            image.src = './img/Partly Cloudy (1).png'
+            break;
+          case 'Cloudy':
+            image.src = './img/Cloudy (1).png';
+            break
+          case 'Overcast':
+            image.src = './img/Overcast (1).png';
+            break
+          case 'Light rain shower':
+            image.src = './img/Light rain (1).png';
+            break
+          case 'Patchy light rain':
+            image.src = './img/Light rain (1).png';
+            break
+          case 'Partly cloudy':
+            image.src = './img/Partly Cloudy (1).png';
+            break
+          default: image.src = './img/Clear.png';
+            break;
+        }
+      } else {
+        switch (data.current.condition.text) {
+          case 'Partly cloudy':
+            image.src = './img/Partly Cloudy.png'
+            break;
+          case 'Cloudy':
+            image.src = './img/Cloudy.png';
+            break
+          case 'Overcast':
+            image.src = './img/Overcast.png';
+            break
+          case 'Light rain shower':
+            image.src = './img/Light rain.png';
+            break
+          case 'Patchy light rain':
+            image.src = './img/Light rain.png';
+            break
+          case 'Partly cloudy':
+            image.src = './img/Partly Cloudy.png';
+            break
+          default: image.src = './img/Sunny.png';
+            break;
+        }
+      }
+
+      setTimeout(() => {
+        content.forEach(block => {
+          block.style.opacity = '1';
+          block.style.scale = '100%';
+        });
+      }, 1000);
+
+    }).catch(error => {
+      console.log(error);
+
+      sectionTrue.style.height = '400px'
+
+      content.forEach(block => {
+        block.style.opacity = '0';
+        block.style.scale = '60%';
+      });
+
+      setTimeout(() => {
+        content.forEach(block => {
+          block.style.opacity = '1';
+          block.style.scale = '100%';
+        });
+      }, 500);
+
+      content[0].style.display = 'none';
+      content[1].style.display = 'flex';
+    })
+}
